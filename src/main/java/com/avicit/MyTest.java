f39f793e08d6949dc8d70a130b36c1e506ef69f1
zhaoc_avic@163.com <passw0rd!>
2019-01-13 22:54:34 +0800

    Initial commit

---
+++ src/main/java/com/avicit/MyTest.java
@@ -0,0 +1,29 @@
+package com.avicit;
+
+import org.junit.Test;
+
+/**
+ * Created by zhaoc on 2019/1/12
+ */
+public class MyTest {
+
+    @Test
+    public void speak(){
+
+        String a = "2";
+        String b = "4";
+        /*String c = Integer.toString(a);
+        String d = Integer.toString(b);*/
+        String e = a+ ""+b;
+        Integer c = Integer.parseInt(a) + Integer.parseInt(b);
+
+        C.TO
+
+        System.out.println(c);
+    }
+
+    @Test
+    public void speak2(){
+        System.out.println("hello world22");
+    }
+}
